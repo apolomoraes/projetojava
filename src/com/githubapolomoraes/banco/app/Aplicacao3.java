61d5ba79c21cfeb75299cd8fe7d88dee4c108f7b
Apolo Moraes <vlogsapolo@gmail.com>
2023-07-22 20:57:40 -0300

    create class NumeroContaFunction

--- src/com/githubapolomoraes/banco/app/Aplicacao3.java
+++ src/com/githubapolomoraes/banco/app/Aplicacao3.java
@@ -3,9 +3,13 @@ package com.githubapolomoraes.banco.app;
 import com.githubapolomoraes.banco.modelo.Banco;
 import com.githubapolomoraes.banco.modelo.Conta;
 
+import java.util.Comparator;
+
 public class Aplicacao3 {
     public static void main(String[] args) {
         Banco banco = new Banco();
+
+        banco.getContas().sort(Comparator.comparingInt());
 
         for (Conta conta : banco.getContas()) {
             System.out.println(conta.getAgencia() + "/" + conta.getNumero());
