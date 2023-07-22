571e81a12c5e20f5f515f9c850f47c4e1e163f82
Apolo Moraes <vlogsapolo@gmail.com>
2023-07-22 19:32:58 -0300

    finally class Aplicacao3 and class Banco

--- src/com/githubapolomoraes/banco/app/Aplicacao3.java
+++ src/com/githubapolomoraes/banco/app/Aplicacao3.java
@@ -1,5 +1,14 @@
 package com.githubapolomoraes.banco.app;
 
-public class Aplicacao3 {
+import com.githubapolomoraes.banco.modelo.Banco;
+import com.githubapolomoraes.banco.modelo.Conta;
 
+public class Aplicacao3 {
+    public static void main(String[] args) {
+        Banco banco = new Banco();
+
+        for (Conta conta : banco.getContas()) {
+            System.out.println(conta.getAgencia() + "/" + conta.getNumero());
+        }
+    }
 }
