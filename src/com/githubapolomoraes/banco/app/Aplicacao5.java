b10f5cc1da9bf0de62d822799e683d20dd3217b3
Apolo Moraes <vlogsapolo@gmail.com>
2023-07-23 19:32:34 -0300

    added Aplicacao5 class

---
+++ src/com/githubapolomoraes/banco/app/Aplicacao5.java
@@ -0,0 +1,11 @@
+package com.githubapolomoraes.banco.app;
+
+import com.githubapolomoraes.banco.modelo.Banco;
+
+public class Aplicacao5 {
+    public static void main(String[] args) {
+        Banco banco = new Banco();
+
+
+    }
+}
--- src/com/githubapolomoraes/banco/modelo/Banco.java
+++ src/com/githubapolomoraes/banco/modelo/Banco.java
@@ -30,4 +30,14 @@ public class Banco {
     }
 
     public List<Conta> getContas() { return contas; }
+
+    public Conta buscar(int agencia, int numeroConta) {
+        for (Conta conta : getContas()) {
+            if (conta.getAgencia() == agencia && conta.getNumero() == numeroConta) {
+                return conta;
+            }
+        }
+
+        return null;
+    }
 }
